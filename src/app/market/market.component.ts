885533d4d0ef71e52b7b5d0205cef83209f98865
gokulnath <gokul@getpitstop.com>
2020-02-29 17:59:42 +0530

    loader adding

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {LoaderService} from '../service/loader.service';
 import { first } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { UUID } from 'angular2-uuid';
@@ -13,7 +14,8 @@ export class MarketComponent implements OnInit {
   getuserdatas: any;
   getname: any = [];
   name: any;
-  constructor(private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, 
+              private loader: LoaderService) { }
 
   addForm: FormGroup;
   submitted = false;
@@ -22,17 +24,21 @@ export class MarketComponent implements OnInit {
   ngOnInit() {
     this.getuserdatas = localStorage.getItem('currentuserdetails');
     if (this.getuserdatas) {
+      this.loader.display(true);
       this.getname.push(JSON.parse(this.getuserdatas));
       console.log(this.getname);
+      this.loader.display(false);
     } else {
       this.router.navigate(['login']);
     }
   }
 
   logout() {
+    this.loader.display(true);
     localStorage.removeItem('currentuserdetails');
     localStorage.clear();
     this.router.navigate(['login']);
+    this.loader.display(false);
 
   }
   get f() { return this.addForm.controls; }
